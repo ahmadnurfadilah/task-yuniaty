5dac65b4c817bc6a327d795144548aeddf720f4b
Ahmad Nurfadilah <ahmadnurfadilah22@gmail.com>
2020-05-07 21:35:22 +0700

    Tambahkan komentar di routes

--- routes/web.php
+++ routes/web.php
@@ -23,6 +23,7 @@ Route::get('/', function () {
 
 Auth::routes();
 
+// Ini komentar
 Route::get('/home', 'HomeController@index')->name('home');
 
 Route::get('/tweet', 'TweetController@index')->middleware('auth');
